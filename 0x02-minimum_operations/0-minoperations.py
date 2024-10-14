ae2ccaf636d3d7880d930f2d737a38170338530c
Katalyst99 <katleho333@gmail.com>
2024-10-14 03:39:38 -0700

    0. Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Algorithm: Minimum Operations"""
+
+
+def minOperations(n):
+    """
+    A method that calculates the fewest number of operations,
+    needed to result in exactly n H characters in the file.
+    """
+    div = 2
+    numOps = 0
+
+    if n <= 1:
+        return 0
+
+    while n > 1:
+        if n % div == 0:
+            n = n / div
+            numOps = numOps + div
+        else:
+            div += 1
+    return numOps

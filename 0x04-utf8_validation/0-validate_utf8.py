45d0c58bdf8a795b6f56c3c092cc39f04f93bcc8
Katalyst99 <katleho333@gmail.com>
2024-10-28 10:51:01 +0000

    Improving code

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """The module for UTF-8 Validation"""
+from typing import List
 
 
-def validUTF8(data):
+def validUTF8(data: List[int]) -> bool:
     """
     Method that determines if a given data set represents a valid UTF-8
     """

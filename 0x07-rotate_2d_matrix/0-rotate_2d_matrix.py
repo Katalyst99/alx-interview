39baa5ba0e1b757ea2468d963602803406136674
Katalyst99 <katleho333@gmail.com>
2024-11-19 05:38:45 +0000

    Corrected checker errors

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -9,9 +9,10 @@ def rotate_2d_matrix(matrix):
     """
     n = len(matrix)
     for i in range(n):
-        for j in range(i + 1, n):
+        for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
     for x in matrix:
         for j in range(n):
             x.reverse()
+    return matrix

01c71c0035315664f5f8084e4f33121123c7de25
Katalyst99 <katleho333@gmail.com>
2024-11-26 10:07:23 +0000

    0. Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""The module for the coin change problem"""
+
+
+def makeChange(coins, total):
+    """Return: fewest number of coins needed to meet total"""
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    change = 0
+    for c in coins:
+        if total == 0:
+            break
+        change += total // c
+        total %= c
+
+    return change if total == 0 else -1
